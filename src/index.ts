58cf452ef8a176b6782f466644cfafc2f8b17f58
Jasper Degens <jasperdegens@gmail.com>
2022-03-08 21:20:30 -0500

    added fun confetti

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import ConfettiGenerator from 'confetti-js'
 
 require('aframe')
 require('./components/movable')
@@ -9,11 +11,23 @@ require('./main.css')
 
 // add threemeshui update block
 window.addEventListener('load', () => {
+    var confettiSettings = { target: 'confetti-canvas',
+        props: [
+            {type: 'svg', src: 'assets/quarterNote.svg'},
+            {type: 'svg', src: 'assets/quarterNotes.svg'}
+        ],
+        rotate: true,
+        size: 3
+    };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
 
     const startButtons = document.getElementsByClassName('experience-start')
     for(const b of startButtons) {
         b.addEventListener('click', (e) => {
             console.log(e)
+            
+            confetti.clear()
         })
     }
 
