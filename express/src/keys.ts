360faeaab6e5effd37664980cae466af4ba2d409
Jasper Degens <jasperdegens@gmail.com>
2022-03-11 16:01:28 -0500

    added local process env

--- express/src/keys.ts
+++ express/src/keys.ts
@@ -1,15 +1,17 @@
-import { ethPrivKey, infuraKey, lighthousePassword, pinataKey, pinataSecret } from './.keys'
 
-const lp = process.env.LIGHTHOUSE || lighthousePassword
-const pk = process.env.PINATA_KEY || pinataKey
-const ps = process.env.PINATA_SECRET || pinataSecret
-const ethPK = process.env.ETH_KEY || ethPrivKey
-const ik = process.env.INFURA || infuraKey
+require('dotenv').config()
+
+const lp = process.env.LIGHTHOUSE!
+const pk = process.env.PINATA_KEY!
+const ps = process.env.PINATA_SECRET!
+const ethPK = process.env.ETH_KEY!
+const ik = process.env.INFURA!
+
 
 export {
     lp as lighthousePassword,
     pk as pinataKey,
-    pinataSecret,
+    ps as pinataSecret,
     ethPK as ethPrivKey,
     ik as infuraKey,
 }
